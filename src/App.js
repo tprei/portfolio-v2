bb0c2c1c2e14b8a02aa6292d82746e8ed845ac63
Thiago Pinheiro <thiagop@usp.br>
2021-09-20 18:11:34 -0300

    add navbar

--- src/App.js
+++ src/App.js
@@ -1,10 +1,13 @@
 import './App.css';
 
+import {Navbar} from './components/Navbar/Navbar'
+
 function App() {
-  return (
-    <>
-    </>
-  );
+    return (
+        <>
+            <Navbar/>
+        </>
+    );
 }
 
 export default App;
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,11 +1,24 @@
+const colors = require('tailwindcss/colors')
+
 module.exports = {
-  purge: ['./src/**/*.{js,jsx,ts,tsx}', './public/index.html'],
-  darkMode: false, // or 'media' or 'class'
-  theme: {
-    extend: {},
-  },
-  variants: {
-    extend: {},
-  },
-  plugins: [],
+    purge: ['./src/**/*.{js,jsx,ts,tsx}', './public/index.html'],
+    darkMode: false, // or 'media' or 'class'
+    theme: {
+        colors: {
+            red: '#E05D5D',
+            orange: '#FFB344',
+            yellow: '#FFF8E5',
+            green: '#00A19D',
+            black: colors.black
+        },
+        fontFamily: {
+            sans: ['Open Sans', 'sans-serif'],
+            serif: ['Montserrat', 'serif']
+        },
+        extend: {},
+    },
+    variants: {
+        extend: {},
+    },
+    plugins: [],
 }
